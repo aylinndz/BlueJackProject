63700aeef37db17cba5b97cb8c8e21905d4e980a
aylinndz <aylin.donmezdeu@outlook.com>
2023-12-24 00:55:17 +0300

    Update Aylin.java
    score

--- src/Aylin.java
+++ src/Aylin.java
@@ -17,3 +17,28 @@ public class Aylin {
     private Card[] computerDeck;
     private Card[] computerBoard;
 }
+
+public Aylin() {
+    makeDecks("gameDeck");
+    makeDecks("player");
+    makeDecks("computer");
+
+    playerGameWon = 0;
+    computerGameWon = 0;
+
+    boolean a = true;
+    while (a) {
+        a = myLoop();
+    }
+
+    String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    appendFile("history.txt", "Player:" + playerGameWon + " - " + "Computer:" + computerGameWon + ", " + date);
+    String[] file = readFile("history.txt").split("\n");
+    if (file.length == 11) {
+        StringBuilder formatFile = new StringBuilder();
+        for (int i = 1; i < file.length; i++) {
+            formatFile.append(file[i]).append("\n");
+        }
+        writeFile("history.txt", formatFile.toString());
+    }
+}
