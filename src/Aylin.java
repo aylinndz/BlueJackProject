c0c7b98d4aa02a5598dd8ae9520a3ac898b9d3ff
aylinndz <aylin.donmezdeu@outlook.com>
2023-12-23 22:35:17 +0300

    Update Aylin.java
    player and computer

--- src/Aylin.java
+++ src/Aylin.java
@@ -1,2 +1,19 @@
+import java.io.FileWriter;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Aylin {
+    private int playerGameWon;
+    private int computerGameWon;
+
+    private Card[] gameDeck;
+    private int gameDeckIndex;
+    private Card[] playerDeck;
+    private Card[] playerBoard;
+    private Card[] computerDeck;
+    private Card[] computerBoard;
 }
