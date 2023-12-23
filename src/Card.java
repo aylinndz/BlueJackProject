98cce24c916cfb1f41d619be512807d5803c7fce
aylinndz <aylin.donmezdeu@outlook.com>
2023-12-23 21:38:12 +0300

    Update Card.java
    Card.

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,17 @@
 public class Card {
+    private boolean flipCard = false;
+    private boolean doubleCard = false;
+
+    private int number;
+    private String colour;
+
+    public Card(boolean flip_card, boolean double_card) {
+        flipCard = flip_card;
+        doubleCard = double_card;
+    }
+    public Card(int num, String color){
+        number = num;
+        colour = color;
+    }
 
 }
